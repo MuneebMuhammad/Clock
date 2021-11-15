c489ce46a2541532d24b852d46f16afe460b25a5
Muneeb <muneebkhattak57@gmail.com>
2021-11-15 19:52:16 +0500

    Function that gives hour, time and seconds is build

--- timer.asm
+++ timer.asm
@@ -1,47 +1,34 @@
 INCLUDE IRVINE32.inc
 
 .data
+totalsec dword 3800
+ts byte ?
+th byte ?
+tm byte ?
 
 .code
 main PROC
+mov eax, totalsec
 
 
 exit
 main endp
+
+;-----------------------------------------------------------------
+findTime PROC uses ecx edx
+; recieves total number of seconds in eax
+; returns seconds, minutes and hour in ts, tm and th respectively
+;-----------------------------------------------------------------
+
+mov edx, 0
+mov ecx, 60
+div ecx
+mov ts, dl
+mov edx, 0
+div ecx
+mov tm, dl
+mov th, al
+
+ret
+findTime endp
 end Main
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
