8abb14440dfbd958be963fa4ceaadadf0834c6d3
Muneeb <muneebkhattak57@gmail.com>
2021-11-21 17:08:21 +0500

    2 options are provided that work correctly: 1 for showing cuttent time and date, 2 for Timer

--- timer.asm
+++ timer.asm
@@ -8,16 +8,32 @@ tm word ?
 tday word ?
 tmonth word ?
 tyear word ?
+prompt1 byte "Press 1 for current date and time", 0
+prompt2 byte "Press 2 for timer",0
+prompth byte "Enter hours for timer: ",0
+promptm byte "Enter minutes for timer: ",0
+prompts byte "Enter seconds for timer",0 
 .code
 main PROC
-call currentTime
+mov edx, offset prompt1
+call writestring
+call crlf
+mov edx, offset prompt2
+call writestring
+call crlf
+call readint
+.IF eax == 1
+	call showMenu
+.ELSEIF eax == 2
+	call Timerfucn
+.ENDIF
 
 EXIT
 main endp
 
 ;-----------------------------------------
-currentTime PROC 
-; displays current time and date
+showMenu PROC 
+; displays menu
 ;-----------------------------------------
 L1:
 call clrscr
@@ -32,15 +48,21 @@ mov tm, ax
 mov ax, sysTime.wSecond
 mov ts, ax
 call displayTime
+call displayDate
+call crlf
+call crlf
+mov edx, offset prompt1
+call writestring
 mov eax, 1000
 call delay
 Loop L1
 ret
-currentTime endp
+showMenu endp
 
 ;--------------------------------------
 displayTime PROC uses eax
-;takes hour in th, minutes in tm and seconds in ts. Then displays time and date
+;takes hour in th, minutes in tm and seconds in ts. 
+;Then displays time and date
 ;--------------------------------------
 mov eax, 0
 mov ax, th
@@ -56,6 +78,13 @@ call writechar
 mov ax, ts
 call single
 call writedec
+ret
+displayTime endp
+
+;---------------------------------------
+displayDate PROC uses eax edx
+; displays current date
+;---------------------------------------
 mov dh, 0
 mov dl, 70
 call gotoxy
@@ -70,7 +99,7 @@ call writechar
 mov ax, sysTime.wYear
 call writedec
 ret
-displayTime endp
+displayDate endp
 
 ;--------------------------------------
 single PROC uses ax
@@ -84,4 +113,38 @@ single PROC uses ax
 ret
 single endp
 
+Timerfucn PROC 
+mov edx, offset prompth
+call writestring
+call readint
+mov th, ax
+mov edx, offset promptm
+call writestring
+call readint
+mov tm, ax
+mov edx, offset prompts
+call writestring
+call readint
+mov ts, ax
+LongT:
+call displayTime
+dec ts
+.IF ts == -1
+	dec tm
+	mov ts, 59
+	.IF tm == -1
+		dec th
+		mov tm, 59
+		.IF th == -1
+			jmp Timerend
+		.ENDIF
+	.ENDIF
+.ENDIF
+mov eax, 1000
+call delay
+call clrscr
+jmp LongT
+Timerend:
+ret
+Timerfucn endp
 end Main
